0d818efe2f254fa9cc4d001cf92563c9335f09b4
veejar89 <rajeev.nov89@gmail.com>
2021-07-12 18:18:52 +0530

    optimized code

--- sumOfNumbers.java
+++ sumOfNumbers.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 public class sumOfNumbers {
   public static void main(String[] args){
-    int totNum, sum;
-    HashMap<String, Integer> numbers = new HashMap<>();
+    int totNum =0 , sum=0;
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter total Numbers to be added: ");
     totNum = sc.nextInt();
     System.out.println("Total Numbers " + totNum);
     for (int a= 1; a <= totNum; a++){
       System.out.println("Enter " + a + "Number ");
-      numbers.put("Key" + a, sc.nextInt());
-      System.out.println(numbers.get("Key" + a));
-    }
-sum =0;
-    for (Map.Entry<String, Integer> entry : numbers.entrySet()){
-      sum = sum + entry.getValue();
+      sum = sum + sc.nextInt();
     }
     System.out.println("Sum of given numbers is " + sum);
   }
