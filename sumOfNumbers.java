ff994fef7a18e5d7280b6a08e0ee47800a52df60
veejar89 <rajeev.nov89@gmail.com>
2021-07-12 18:22:05 +0530

    optimized code

--- sumOfNumbers.java
+++ sumOfNumbers.java
@@ -4,7 +4,7 @@ public class sumOfNumbers {
   public static void main(String[] args){
     int totNum =0 , sum=0;
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter total Numbers to be added: ");
+    System.out.println("Enter Total Numbers to be added: ");
     totNum = sc.nextInt();
     System.out.println("Total Numbers " + totNum);
     for (int a= 1; a <= totNum; a++){
