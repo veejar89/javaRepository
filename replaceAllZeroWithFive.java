4b27c520dc5a4a3166900d91af63abbf5e57d410
veejar89 <rajeev.nov89@gmail.com>
2021-07-13 13:28:59 +0530

    With Question

--- replaceAllZeroWithFive.java
+++ replaceAllZeroWithFive.java
@@ -1,3 +1,13 @@
+/*You are given an integer N. You need to convert all zeroes of N to 5.
+'Example 1:
+'Input:
+'N = 1004
+'Output: 1554
+'Explanation: There are two zeroes in 1004
+'on replacing all zeroes with "5", the new
+'number will be "1554".*/
+
+
 import java.util.*;
 
 public class replaceAllZeroWithFive {
